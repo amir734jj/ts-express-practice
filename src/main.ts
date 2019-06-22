8ce8d35a960805dd3bd8d3498b951e91d4cbf675
Amir <hesamian@uwm.edu>
2019-06-21 23:52:46 -0500

    started work

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,62 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
-let bodyParser = require('body-parser');
-// import cookieParser from 'cookie-parser';
-// import expressSession from 'express-session';
-// import connectSessionSequelize from 'connect-session-sequelize';
+import * as cookieParser from 'cookie-parser';
 import {Sequelize} from 'sequelize-typescript';
 import { User } from './models/user';
-// connectSessionSequelize(expressSession.Store);
+import * as bodyParser from 'body-parser';
+import * as expressSession from 'express-session';
+import * as connectSessionSequelize from 'connect-session-sequelize';
+
+let sequelizeSessionStore = connectSessionSequelize(expressSession.Store);
+let sequelize = new Sequelize({
+    host: 'localhost',
+    database: 'ts_express',
+    dialect: 'sqlite',
+    storage: 'db.sqlite',
+    logging: false,
+    pool: {
+        min: 0,
+        max: 1,
+        idle: 10000
+    }
+});
+
+sequelize.addModels([User]);
 
 const app = express();
 const port = 3000;
 app.locals.pretty = true;
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('views', 'views');
+app.set('view engine', 'pug');
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'keyboard cat',
+    store: new sequelizeSessionStore({
+        db: sequelize
+    }),
+    resave: false,  // we support the touch method so per the express-session docs this should be set to false
+    proxy: true     // if you do SSL outside of node.
+}));
 
+app.get('/', async (_: Request, res: Response) => {
+    // const person = User.build({
+    //     name: 'boo',
+    //     username: 'bar',
+    //     password: 'baz'
+    // } as User);
+    //
+    // await person.save();
 
-let sequelize = new Sequelize({
-    database: 'some_db',
-    dialect: 'sqlite',
-    storage: 'database.sqlite'
+    res.render('index');
 });
-
-sequelize.addModels([User]);
 
 sequelize.sync({ force: true }).then();
-
-app.get('/', (_: Request, res: Response) => {
-    const person = User.build({
-        name: 'boo',
-        username: 'bar',
-        password: 'baz'
-    } as User);
-    person.save().then();
-
-    res.send('Hello world!');
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
