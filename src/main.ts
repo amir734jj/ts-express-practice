97033f930d3b7db4cd6d40ff8248fc976114654c
Amir <hesamian@uwm.edu>
2019-07-12 21:59:09 -0500

    no sure

--- src/main.ts
+++ src/main.ts
@@ -43,6 +43,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(expressSession({
+    saveUninitialized: true,
     secret: 'keyboard cat',
     store: new sequelizeSessionStore({
         db: sequelize,
